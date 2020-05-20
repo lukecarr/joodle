00b7186c8124c680a7b6207a42c48c88ad2e0529
Luke Carr <me@lukecarr.dev>
2020-05-20 20:08:53 +0100

    feat(auth.email): added response typings to getSignUpSettings()
    Also added missing JSDocs to most classes and functions

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,20 @@
 import got, { Got } from "got";
 
 export interface ClientOptions {
+  /**
+   * The base URL of the Moodle site to send API requests to.
+   */
   baseURL: string;
+
+  /**
+   * The token used to authenticate with the Moodle site's Web Services API.
+   */
   token: string;
 }
 
+/**
+ * A client that can send HTTP requests to a Moodle site's Web Services API.
+ */
 export abstract class Client {
   private baseURL: string;
 
@@ -12,6 +22,12 @@ export abstract class Client {
 
   public got: Got;
 
+  /**
+   * Initializes the client as well as the client's `got` instance so HTTP
+   * requests can be made.
+   * 
+   * @param options The client's configuration options.
+   */
   public constructor(options: ClientOptions) {
     this.baseURL = options.baseURL;
     this.token = options.token;
--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -2,6 +2,9 @@ import Module from "../../module";
 import AuthEmailModule from "./email";
 import { Client } from "../../client";
 
+/**
+ * Functions relating to authentication in Moodle (specifically self-registration).
+ */
 export default class AuthModule extends Module {
   public email: AuthEmailModule;
 
