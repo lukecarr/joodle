855c5d1bd484fa0615eadbc78a9633c428e1e32a
Luke Carr <me@lukecarr.dev>
2020-05-20 20:10:29 +0100

    chore: version bump

--- src/modules/auth/email.ts
+++ src/modules/auth/email.ts
@@ -5,18 +5,18 @@ export interface SignUpSettingsResponse {
    * The fields that a user can provide during sign up to identify their name.
    */
   namefields: string[];
-  
+
   /**
    * Users signing up to this Moodle site must use a password that meets this policy's requirements.
    */
   passwordpolicy: string;
-  
+
   warnings: unknown[];
 }
 
 /**
  * Functions relating to Moodle's email-based self-registration.
- * 
+ *
  * This module's function calls may throw errors if self registration is disabled.
  */
 export default class AuthEmailModule extends Module {
@@ -24,6 +24,8 @@ export default class AuthEmailModule extends Module {
    * Get the sign-up required settings and profile fields.
    */
   public async getSignUpSettings(): Promise<SignUpSettingsResponse> {
-    return (await this.get("auth_email_get_signup_settings")) as SignUpSettingsResponse;
+    return (await this.get(
+      "auth_email_get_signup_settings"
+    )) as SignUpSettingsResponse;
   }
 }
