00dd783ac6872e40f6f1eeecc646023fe4c1adbf
Luke Carr <me@lukecarr.dev>
2020-05-21 19:14:25 +0100

    feat(modules): added core_user_get_course_user_profiles function

---
+++ src/modules/core/user/get-course-user-profiles.ts
@@ -0,0 +1,102 @@
+import { FunctionResponse } from "../../../functions";
+import { User } from "../../shared";
+
+export interface CourseUser {
+  /**
+   * The ID of the user.
+   */
+  userid: number;
+
+  /**
+   * The ID of the course.
+   */
+  courseid: number;
+}
+
+interface UserGroup {
+  /**
+   * The group's unique ID.
+   */
+  id: number;
+
+  /**
+   * The group's name.
+   */
+  name: string;
+
+  /**
+   * The group's description.
+   */
+  description: string;
+
+  /**
+   * The group's description format code.
+   *
+   * `0` - Moodle
+   * `1` - HTML
+   * `2` - Plain text
+   * `4` - Markdown
+   */
+  descriptionformat: number;
+}
+
+interface UserRole {
+  /**
+   * The role's unique ID.
+   */
+  roleid: number;
+
+  /**
+   * The role's name.
+   */
+  name: string;
+
+  /**
+   * The role's shortname.
+   */
+  shortname: string;
+
+  /**
+   * The role's sorting order.
+   */
+  sortorder: number;
+}
+
+interface EnrolledCourse {
+  /**
+   * The course's unique ID.
+   */
+  id: number;
+
+  /**
+   * The course's full name.
+   */
+  fullname: string;
+
+  /**
+   * The course's shortname.
+   */
+  shortname: string;
+}
+
+interface CourseUserProfile extends User {
+  /**
+   * The user's groups.
+   */
+  groups?: UserGroup[];
+
+  /**
+   * The user's roles.
+   */
+  roles?: UserRole[];
+
+  /**
+   * The courses that the user is
+   * enrolled in.
+   */
+  enrolledcourses?: EnrolledCourse[];
+}
+
+export interface GetCourseUserProfilesResponse extends FunctionResponse {
+  profiles: CourseUserProfile[];
+}
