d763ac1fb431cfa0b70a34071a405567cb35676e
Luke Carr <me@lukecarr.dev>
2020-05-21 09:36:58 +0100

    feat(modules): added getHttpResponse() to get raw HTTP response

---
+++ src/functions/response.ts
@@ -0,0 +1,13 @@
+import { Response } from "got";
+
+/**
+ * The response returned by a function call
+ * to Moodle's Web Services API.
+ */
+export default interface FunctionResponse {
+  /**
+   * Returns the raw HTTP response returned
+   * from the API call for this function.
+   */
+  getHttpResponse(): Response;
+}
